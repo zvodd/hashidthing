e2a885c7be131c5d12925e3b3a8c8afe9214a64c
zvodd <zv.odd.101@gmail.com>
2020-07-11 12:54:22 +0000

    works

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 
 import './App.css';
+import HashIDStrings from './hashthing'
 
 const App = () => {
+    const teststring = "123Test"
+    const thehash = HashIDStrings.stringToHashID(teststring)
     return (
       <div className="App">
         <header className="App-header">
@@ -12,6 +15,12 @@ const App = () => {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+                    <h1>{teststring}</h1>
+                    <h2>{HashIDStrings.hashIDToInts(thehash).join(',')}</h2>
+                    <h2>{thehash}</h2>
+                    <h2>{HashIDStrings.hashIDToString(thehash)}</h2>
         </p>
       </div>
     );
--- src/hashthing.js
+++ src/hashthing.js
@@ -4,27 +4,32 @@ import Hashids from 'hashids'
 const CodeCharacters = [ '2', '3', '4', '5', '6', '7', '8', '9', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'T', 'V', 'W', 'X', 'Y' ];
 const HashCharsStr = CodeCharacters.join('')
 
-const hashids = new Hashids('',0,)
+const hashids = new Hashids('', 0, HashCharsStr)
 
 
 const stringsToInts = (str)=>{
-    return str.split('').map(c=> c.charCodeAt(i))
+    return str.split('').map(c=> c.charCodeAt(0))
 }
 const intsToString =  (ints) =>{
     return ints.map(i => String.fromCharCode(i)).join('')
 }
 
 const stringToHashID = (str) =>{
-    return hashids.encode(charsToInts(str))
+    return hashids.encode(stringsToInts(str))
 }
 const hashIDToString = (hash) =>{
     return intsToString(hashids.decode(hash))
 }
+const hashIDToInts = (hash) =>{
+    return hashids.decode(hash)
+}
 
-
-export default {
+const HashIDStrings = {
+    intsToString,
+    stringsToInts,
     stringToHashID,
     hashIDToString,
-    stringsToInts,
-    intsToString
+    hashIDToInts
 }
+
+export {HashIDStrings as default}
